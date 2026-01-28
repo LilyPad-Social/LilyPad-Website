0c4805c735af8ed5424e5533942a7354b6cb3d1d
PengMouse <tundejoseph294@gmail.com>
2026-01-28 11:49:36 +0100

    feat: fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,7 @@ export default function Document() {
 				<meta property="og:url" content="https://lilypad.benagos.com/" />
 				<meta property="og:author" name="Afolabi Babatunde Joseph" />
 				<meta property="og:author" name="Ehizojie Ihayere" />
+				<meta name="og:image" content="https://i.ibb.co/fGYrKSqX/favicon.jpg" />
 
 				{/* Twitter Cards */}
 				<meta name="twitter:card" content="summary_large_image" />
